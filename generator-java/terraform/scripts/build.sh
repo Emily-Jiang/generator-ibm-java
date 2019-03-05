9d620ef0b50cff33b00ed143a7babc3f007d83e5
Emily Jiang <emijiang@uk.ibm.com>
2019-03-05 17:20:26 -0500

    Revert "update Liberty version and MP version"
    This reverts commit d2821806ce1a44af80ea136b432f40de52e76580.

--- generator-java/terraform/scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-mvn clean install
-sed -i 's/default.http.port=9080/default.http.port=3000/g' target/liberty/wlp/usr/servers/defaultServer/bootstrap.properties
-target/liberty/wlp/bin/server package defaultServer --archive="myapp" --include=minify
--- generator-java/terraform/scripts/start.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-pkill java
-unzip myapp.zip
-wlp/bin/server start
